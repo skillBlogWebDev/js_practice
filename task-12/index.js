050842d76daf5e8505712ac06c4009ea31157953
Negan-ai <yoosyoos32@gmail.come>
2021-10-16 18:10:24 +0300

    #12

---
+++ task-12/index.js
@@ -0,0 +1,34 @@
+let intervalId;
+
+document.querySelectorAll('.dropdown-toggle').forEach(e => {
+    e.addEventListener('click', e => {
+        const menu = e.currentTarget.dataset.path;
+        document.querySelectorAll('.dropdown-menu').forEach(e => {
+            if (!document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+                e.classList.remove('menu-active');
+                e.classList.remove('open');
+                document.querySelector(`[data-target=${menu}]`).classList.add('menu-active');
+                intervalId = setTimeout(() => {
+                    document.querySelector(`[data-target=${menu}]`).classList.add('open');
+                }, 0);
+            }
+
+            if (document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+                clearTimeout(intervalId);
+                document.querySelector(`[data-target=${menu}]`).classList.remove('menu-active');
+                intervalId = setTimeout(() => {
+                    document.querySelector(`[data-target=${menu}]`).classList.remove('open');
+                }, 0);
+            }
+
+            window.onclick = e => {
+                if (e.target == document.querySelector(`[data-target=${menu}]`) || e.target == document.querySelector(`[data-path=${menu}]`)) {
+                    return;
+                } else {
+                    document.querySelector(`[data-target=${menu}]`).classList.remove('menu-active');
+                    document.querySelector(`[data-target=${menu}]`).classList.remove('open');
+                }
+            }
+        });
+    });
+});
